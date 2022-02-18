e51151d2c2c15d59e9b8d7d4d2877d8be1221797
Abilkair <hurricane_axe@mail.ru>
2022-02-18 22:58:22 +0600

    #1

--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,48 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type myside int
+
+func CalcSquare(sideLen float64, sidesNum myside) float64 {
+
+	var result float64
+
+	if sidesNum == 0 {
+		result = CalcCircle(sideLen)
+	} else if sidesNum == 3 {
+		result = CalcTriangle(sideLen)
+	} else if sidesNum == 4 {
+		result = CalcRectangle(sideLen)
+	} else {
+		result = 0.0
+	}
+	
+	return result
+}
+
+func CalcRectangle(sideLen float64) float64 {
+
+	var RectangleSquare float64
+
+	RectangleSquare = sideLen * 2
+
+	return RectangleSquare
+}
+
+func CalcTriangle(sideLen float64) float64 {
+
+	var TriangleSquare float64
+
+	TriangleSquare = (sideLen * 2 * math.Sqrt(3.0)) / 4
+
+	return TriangleSquare
+}
+
+func CalcCircle(sideLen float64) float64 {
+
+	var CircleSquare float64
+
+	CircleSquare = sideLen * 2 * math.Pi
+
+	return CircleSquare
 }
